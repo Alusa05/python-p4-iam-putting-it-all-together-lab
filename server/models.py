2849da39c211a96cadf98b1cef2254a01f52b2a3
Lisa Alusa <sayilisaalusa@gmail.com>
2025-06-26 13:38:30 +0300

    changes

--- server/models.py
+++ server/models.py
@@ -7,7 +7,15 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_harsh = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship("Recipe", backref="user", lazy=True)
+
+    
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
